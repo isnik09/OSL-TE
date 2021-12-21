27c24a8850d7fba10452a1939083c284e6e55037
NIK2221994 <63090536+NIK2221994@users.noreply.github.com>
2021-12-21 07:50:23 +0530

    Create FORK SYSTEM CALL EXECVE.c
    Implement the C program in which main program accepts an array. Main program uses the FORK system call to create a new process called a child process. Parent process sorts an array and passes the sorted array to child process through the command line arguments of EXECVE system call. The child process uses EXECVE system call to load new program which display array in reverse order.

---
+++ FORK SYSTEM CALL EXECVE.c
@@ -0,0 +1,66 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+int main(int argc,char *argv[])
+{
+
+ int val[10],ele;
+ pid_t pid;
+ char *cval[10];
+ int i,j,n,temp;
+ printf("\n Enter the size of array\n");
+ scanf("%d",&n);
+ printf("\n Enter the numbers\n");
+ for(i=0;i<n;i++)
+  {
+   scanf("%d",&val[i]);
+  }
+ printf("\n Elements entered are\n");
+ for(i=0;i<n;i++)
+ printf("%d",val[i]);
+ {
+ 
+ 
+  for(i=1;i<n;i++)
+  {
+   for(j=0;j<n-1;j++)
+   {
+    if(val[j]>val[j+1])
+    {
+     temp=val[j];
+     val[j]=val[j+1];
+     val[j+1]=temp;
+    }
+   }
+  }
+ 
+
+ printf("\n Sorted elements are\n:");
+
+ for(i=0;i<n;i++)
+ {
+  printf("%d",val[i]);
+ }
+  
+ printf("\n Enter element to search:");
+ scanf("%d",&ele);
+ val[i]=ele;
+ for(i=0;i<n+1;i++)
+ {
+ 
+  char a[sizeof(int)];
+  snprintf(a,sizeof(int),"%d",val[i]);
+  cval[i]=malloc(sizeof(a));
+  strcpy(cval[i],a);
+ }
+ cval[i]=NULL;
+ pid=fork();
+ if(pid==0)
+ { 
+  execv(argv[1],cval);
+  perror("\n Error in execv call");
+ }
+
+}
+}
